b48b80242b24fefc5839e29d7543b25eeaff5b46
Jan Recinsky <jan.recinsky@ackee.cz>
2015-02-22 19:03:18 +0100

    add wifi client

--- wifiserver/wifiserver.ino
+++ wifiserver/wifiserver.ino
@@ -99,51 +99,21 @@ char path[MAX_PATH+1];
 
 void setup(void)
 {
-  Serial.begin(115200);
-  Serial.println(F("Hello, CC3000!\n")); 
-
-  Serial.print("Free RAM: "); Serial.println(getFreeRam(), DEC);
+ 
   
-  // Initialise the module
-  Serial.println(F("\nInitializing..."));
-  if (!cc3000.begin())
-  {
-    Serial.println(F("Couldn't begin()! Check your wiring?"));
-    while(1);
-  }
+  cc3000.begin();
+ 
   
-  Serial.print(F("\nAttempting to connect to ")); Serial.println(WLAN_SSID);
-  if (!cc3000.connectToAP(WLAN_SSID, WLAN_PASS, WLAN_SECURITY)) {
-    Serial.println(F("Failed!"));
-    while(1);
-  }
+  cc3000.connectToAP(WLAN_SSID, WLAN_PASS, WLAN_SECURITY);
    
-  Serial.println(F("Connected!"));
-  
-  Serial.println(F("Request DHCP"));
+   
   while (!cc3000.checkDHCP())
   {
     delay(100); // ToDo: Insert a DHCP timeout!
   }  
 
-  // Display the IP address DNS, Gateway, etc.
-  while (! displayConnectionDetails()) {
-    delay(1000);
-  }
-
-  // ******************************************************
-  // You can safely remove this to save some flash memory!
-  // ******************************************************
-  Serial.println(F("\r\nNOTE: This sketch may cause problems with other sketches"));
-  Serial.println(F("since the .disconnect() function is never called, so the"));
-  Serial.println(F("AP may refuse connection requests from the CC3000 until a"));
-  Serial.println(F("timeout period passes.  This is normal behaviour since"));
-  Serial.println(F("there isn't an obvious moment to disconnect with a server.\r\n"));
-  
-  // Start listening for connections
   httpServer.begin();
   
-  Serial.println(F("Listening for connections..."));
 }
 
 void loop(void)
@@ -246,24 +216,3 @@ void parseFirstLine(char* line, char* action, char* path) {
     strncpy(path, linepath, MAX_PATH);
 }
 
-// Tries to read the IP address and other connection details
-bool displayConnectionDetails(void)
-{
-  uint32_t ipAddress, netmask, gateway, dhcpserv, dnsserv;
-  
-  if(!cc3000.getIPAddress(&ipAddress, &netmask, &gateway, &dhcpserv, &dnsserv))
-  {
-    Serial.println(F("Unable to retrieve the IP Address!\r\n"));
-    return false;
-  }
-  else
-  {
-    Serial.print(F("\nIP Addr: ")); cc3000.printIPdotsRev(ipAddress);
-    Serial.print(F("\nNetmask: ")); cc3000.printIPdotsRev(netmask);
-    Serial.print(F("\nGateway: ")); cc3000.printIPdotsRev(gateway);
-    Serial.print(F("\nDHCPsrv: ")); cc3000.printIPdotsRev(dhcpserv);
-    Serial.print(F("\nDNSserv: ")); cc3000.printIPdotsRev(dnsserv);
-    Serial.println();
-    return true;
-  }
-}
