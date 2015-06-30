8e360e76a9f7f4c9944a2efaa4d6fe82f910c9eb
Jan Recinsky <jan.recinsky@ackee.cz>
2015-07-01 01:13:47 +0200

    parsing values

--- home-aut/lib/deamon/read_deamon.rb
+++ home-aut/lib/deamon/read_deamon.rb
@@ -35,6 +35,8 @@
 # $max.times { MyWorker.perform_async }
 
 require 'curb'
+require 'rubygems'
+require 'json'
 ENV['RAILS_ENV'] = "development" # Set to your desired Rails environment name
 require '/Users/Honza/Documents/PROGRAMING/PRIVATE/home-monitor/home-aut/config/environment.rb'
 class Test 
@@ -45,9 +47,24 @@ class Test
     http = Curl.get("192.168.88.250")
     puts http.header_str[0]
 
+  str = "{\"wind\": {\"speed\": 0.00,\"dir\": 315},\"rain\": {\"total\":1.12},\"air\": {\"pressure\": 24, \"humidity\": 58.60,\"temperature\": 21.60},\"lightning\": { \"distance\": \"\"}}"
+
+  parsed = JSON.parse(str)
+puts "<<<AIR>>>>"
+puts parsed["air"]["temperature"]
+puts parsed["air"]["humidity"]
+puts parsed["air"]["pressure"]
+puts "<<<WIND>>>>"
+puts parsed["wind"]["speed"]
+puts parsed["wind"]["dir"]
+puts "<<<RAIN>>>>"
+puts parsed["rain"]["total"]
+
 
     alert = Air.new do |a| 
-    a.temperature = 80 
+    a.temperature = parsed["air"]["temperature"]
+    a.humidity = parsed["air"]["humidity"]
+    a.pressure = parsed["air"]["pressure"]
     end
     alert.save
 
